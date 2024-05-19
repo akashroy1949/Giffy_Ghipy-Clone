bf160e4b22411d94d712a435393d71999a7c148b
Akash <aroy933050@gmail.com>
2024-05-19 15:20:52 +0530

    created context, dynamic render of categories header

---
+++ src/components/header.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
+import { GifState } from "../context/gifContext";
+
+const Header = () => {
+  const [categories, setCategories] = useState();
+  const [showCategories, setShowCategories] = useState();
+
+  const { gifEndpoint, filter, setFilter, favorites } = GifState();
+
+  const fetchGifCategories = async () => {
+    const { data } = await gifEndpoint.categories();
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    fetchGifCategories();
+  }, []);
+
+  return (
+    <nav>
+      <div className="relative flex gap-4 justify-between items-center mb-2">
+        {/* Logo & Title */}
+        <Link to="/" className="flex gap-2">
+          <img
+            src="https://media3.giphy.com/media/l3vR16pONsV8cKkWk/200w.gif?cid=6c09b95250rkwqe202a5xaq59gtohuq4fric3a6bsttwh8as&ep=v1_gifs_search&rid=200w.gif&ct=g"
+            alt="Giphy Clone Logo"
+            className="w-12"
+          />
+          <h1 className="text-5xl font-bold tracking-tight cursor-pointer">
+            GIPHY
+          </h1>
+        </Link>
+
+        <div className="font-bold flex gap-2 text-md items-center">
+          {/* Category List in Large Screens */}
+          {categories?.slice(0, 5)?.map((category) => {
+            return (
+              <Link
+                key={category.name}
+                to={`/${category.name_encoded}`}
+                className="px-4 py-1 border-b-4 hover:gradient hidden lg:inline-block"
+              >
+                {category?.name}
+              </Link>
+            );
+          })}
+          <button onClick={() => setShowCategories(!showCategories)}>
+            <HiOutlineDotsVertical
+              size={35}
+              className={`py-0.5 border-b-4 hover:gradient hidden lg:inline-block ${
+                showCategories ? "gradient" : ""
+              }`}
+            />
+          </button>
+
+          {/* Favorites Button */}
+          {favorites.length > 0 && (
+            <div className="h-9 px-6 pt-1.5 bg-slate-500 rounded bg-opacity-70 cursor-pointer">
+              <Link to={"/favorites"}>Favorite GIFs</Link>
+            </div>
+          )}
+
+          {/* Mobile Category Button */}
+          <button onClick={() => setShowCategories(!showCategories)}>
+            <HiMiniBars3BottomRight
+              size={35}
+              className="text-sky-500 block lg:hidden"
+            />
+          </button>
+        </div>
+
+        {/* Categories List Block */}
+        {showCategories && (
+          <div className="absolute w-full right-0 top-14 px-10 py-6 gradient z-20">
+            <h3 className="text-3xl font-bold">Categories</h3>
+            <hr className="bg-gray-100 opacity-50 my-3" />
+            <div className="text-md grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+              {categories?.map((category) => {
+                return (
+                  <Link
+                    key={category.name}
+                    to={`/${category.name_encoded}`}
+                    className="font-medium opacity-80"
+                  >
+                    {category.name}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+
+      {/* Search */}
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/context/gifContext.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { createContext, useContext, useState } from "react";
+
+const GifContext = createContext();
+
+// provider component
+const GifProvider = ({ children }) => {
+  const [gifs, setGifs] = useState([]);
+  const [filters, setFilters] = useState("gifs");
+  const [favorites, setFavorites] = useState([]);
+
+  const gifEndpoint = new GiphyFetch(import.meta.env.VITE_GIPHY_API_KEY);
+  return (
+    <GifContext.Provider
+      value={{ gifEndpoint, gifs, setGifs, filters, setFilters, favorites }}
+    >
+      {children}
+    </GifContext.Provider>
+  );
+};
+
+export const GifState = () => {
+  return useContext(GifContext);
+};
+export default GifProvider;
