f0c5ce203e94c0bf079a7446de3a2236f4e80de6
akashroy <akashroy8@deloitte.com>
2024-08-14 22:42:08 +0530

    created followOn comp and updated Categroy relevant code

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/gifContext";
+import Gif from "../components/gif";
+import { FollowOn } from "../components/follow-on";
 
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+  const [results, setResults] = useState([]);
+  const { gifEndpoint } = GifState();
+  const { catergory } = useParams();
 
-export default Category
+  const fetchCategoryResults = useCallback(async () => {
+    const { data } = await gifEndpoint.gifs(catergory, catergory);
+    setResults(data);
+  }, [catergory, gifEndpoint]);
+
+  useEffect(() => {
+    fetchCategoryResults();
+  }, [fetchCategoryResults]);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-5 my-4">
+      <div className="w-full sm:w-72">
+        {results.length > 0 && <Gif gif={results[0]} hover={false} />}
+        <span className="text-gray-400 text-sm pt-2">
+          Don&apos;t tell it to me, Gif it to me
+        </span>
+        <FollowOn />
+        <div className="divider" />
+      </div>
+    </div>
+  );
+};
+
+export default Category;
