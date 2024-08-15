6bfc673ea2755932a7410923f51302ca66cd4d6f
akashroy <akashroy8@deloitte.com>
2024-08-16 01:03:36 +0530

    Updated code in gif component & Added code to get data for single  gif

--- src/pages/singleGifPage.jsx
+++ src/pages/singleGifPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/gifContext";
+
+const contentType = ["gifs", "stickers", "texts"];
 
 const SingleGifPage = () => {
-  return (
-    <div>SingleGifPage</div>
-  )
-}
+  const [gif, setGif] = useState({});
+  const [relatedGifs, setRelatedGifs] = useState([]);
+  const { type, slug } = useParams();
+  const { gifEndpoint } = GifState();
 
-export default SingleGifPage
+  const fetchGif = useCallback(async () => {
+    const gifId = slug.split("-");
+    const { data } = await gifEndpoint.gif(gifId[gifId.length - 1]);
+    const { data: related } = await gifEndpoint.related(
+      gifId[gifId.length - 1],
+      {
+        limit: 10,
+      }
+    );
+    setGif(data);
+    setRelatedGifs(related);
+  }, [gifEndpoint, slug]);
+
+  useEffect(() => {
+    if (!contentType.includes(type)) {
+      throw new Error("Invalid Content Tpye");
+    }
+    fetchGif();
+  }, [fetchGif, type]);
+
+  return <div>SingleGifPage{console.log(gif)}<p>This is paragrapgh</p></div>;
+};
+
+export default SingleGifPage;
